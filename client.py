4752f10e873c8d23cafd8e2105a73dde204d6fdb
pofice <111642288+pofice@users.noreply.github.com>
2023-11-22 12:02:49 +0800

    Update client.py

--- client.py
+++ client.py
@@ -16,25 +16,46 @@ TOOL_PROMPT = 'Answer the following questions as best as you can. You have acces
 MODEL_PATH = os.environ.get('MODEL_PATH', 'THUDM/chatglm3-6b')
 PT_PATH = os.environ.get('PT_PATH', None)
 TOKENIZER_PATH = os.environ.get("TOKENIZER_PATH", MODEL_PATH)
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# for Mac Computer like M1
+# You Need Use Pytorch compiled with Metal
+# DEVICE = 'mps'
+
+# for AMD gpu likes MI100 (Not Official Steady Support yet)
+# You Need Use Pytorch compiled with ROCm
+# DEVICE = 'cuda'
+
+# for Intel gpu likes A770 (Not Official Steady Support yet)
+# You Need Use Pytorch compiled with oneDNN and install intel-extension-for-pytorch
+# import intel_extension_for_pytorch as ipex
+# DEVICE = 'xpu'
+
+# for Moore Threads gpu like MTT S80 (Not Official Steady Support yet)
+# You Need Use Pytorch compiled with Musa
+# DEVICE = 'musa'
+
 
 @st.cache_resource
 def get_client() -> Client:
-    client = HFClient(MODEL_PATH, TOKENIZER_PATH, PT_PATH)
+    client = HFClient(MODEL_PATH, TOKENIZER_PATH, PT_PATH, DEVICE)
     return client
+
 
 class Client(Protocol):
     def generate_stream(self,
-        system: str | None,
-        tools: list[dict] | None,
-        history: list[Conversation],
-        **parameters: Any
-    ) -> Iterable[TextGenerationStreamResponse]:
+                        system: str | None,
+                        tools: list[dict] | None,
+                        history: list[Conversation],
+                        **parameters: Any
+                        ) -> Iterable[TextGenerationStreamResponse]:
         ...
 
+
 def stream_chat(self, tokenizer, query: str, history: list[tuple[str, str]] = None, role: str = "user",
-                    past_key_values=None, max_length: int = 8192, do_sample=True, top_p=0.8, temperature=0.8,
-                    logits_processor=None, return_past_key_values=False, **kwargs):
-        
+                past_key_values=None, max_length: int = 8192, do_sample=True, top_p=0.8, temperature=0.8,
+                repetition_penalty=1.0, length_penalty=1.0, num_beams=1,
+                logits_processor=None, return_past_key_values=False, **kwargs):
     from transformers.generation.logits_process import LogitsProcessor
     from transformers.generation.utils import LogitsProcessorList
 
@@ -52,8 +73,18 @@ def stream_chat(self, tokenizer, query: str, history: list[tuple[str, str]] = No
     logits_processor.append(InvalidScoreLogitsProcessor())
     eos_token_id = [tokenizer.eos_token_id, tokenizer.get_command("<|user|>"),
                     tokenizer.get_command("<|observation|>")]
-    gen_kwargs = {"max_length": max_length, "do_sample": do_sample, "top_p": top_p,
-                    "temperature": temperature, "logits_processor": logits_processor, **kwargs}
+    gen_kwargs = {"max_length": max_length,
+                  "do_sample": do_sample,
+                  "top_p": top_p,
+                  "temperature": temperature,
+                  "logits_processor": logits_processor,
+                  "repetition_penalty": repetition_penalty,
+                  "length_penalty": length_penalty,
+                  "num_beams": num_beams,
+                  **kwargs
+                  }
+
+    print(gen_kwargs)
     if past_key_values is None:
         inputs = tokenizer.build_chat_input(query, history=history, role=role)
     else:
@@ -98,8 +129,9 @@ def stream_chat(self, tokenizer, query: str, history: list[tuple[str, str]] = No
             else:
                 yield response, new_history
 
+
 class HFClient(Client):
-    def __init__(self, model_path: str, tokenizer_path: str, pt_checkpoint: str | None = None,):
+    def __init__(self, model_path: str, tokenizer_path: str, pt_checkpoint: str | None = None, DEVICE = 'cpu'):
         self.model_path = model_path
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, trust_remote_code=True)
 
@@ -116,18 +148,15 @@ class HFClient(Client):
         else:
             self.model = AutoModel.from_pretrained(model_path, trust_remote_code=True)
 
-        self.model = self.model.to(
-            'cuda' if torch.cuda.is_available() else
-            'mps' if torch.backends.mps.is_available() else
-            'cpu'
-        ).eval()
+        self.model = self.model.to(DEVICE).eval() if 'cuda' in DEVICE else self.model.float().to(DEVICE).eval()
+
 
     def generate_stream(self,
-        system: str | None,
-        tools: list[dict] | None,
-        history: list[Conversation],
-        **parameters: Any
-    ) -> Iterable[TextGenerationStreamResponse]:
+                        system: str | None,
+                        tools: list[dict] | None,
+                        history: list[Conversation],
+                        **parameters: Any
+                        ) -> Iterable[TextGenerationStreamResponse]:
         chat_history = [{
             'role': 'system',
             'content': system if not tools else TOOL_PROMPT,
@@ -141,19 +170,19 @@ class HFClient(Client):
                 'role': str(conversation.role).removeprefix('<|').removesuffix('|>'),
                 'content': conversation.content,
             })
-        
+
         query = history[-1].content
         role = str(history[-1].role).removeprefix('<|').removesuffix('|>')
 
         text = ''
-        
+
         for new_text, _ in stream_chat(self.model,
-            self.tokenizer,
-            query,
-            chat_history,
-            role,
-            **parameters,
-        ):
+                                       self.tokenizer,
+                                       query,
+                                       chat_history,
+                                       role,
+                                       **parameters,
+                                       ):
             word = new_text.removeprefix(text)
             word_stripped = word.strip()
             text = new_text
